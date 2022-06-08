4a86c45955cfb212bda420b760b8b3352fab9d8c
Ryu Juheon <saidbysolo@gmail.com>
2022-06-08 21:22:37 +0900

    fix: migration to neko-love (#3)
    Close #2

--- hiyobot/commands/anekos.py
+++ hiyobot/commands/anekos.py
@@ -5,90 +5,31 @@ from hiyobot.handler.register import CommandArgument, RegisterCommand
 from hiyobot.utils import is_nsfw
 from random import choice
 
-# dummy ["v3","nekoapi_v3.1"]
 
-nsfw_tags_literal = Literal[
-    "Random_hentai_gif",
-    "cum_jpg",
-    "futanari",
-    "femdom",
-    "smallboobs",
-    "anal",
-    "nsfw_neko_gif",
-    "pussy_jpg",
-    "nsfw_avatar",
-    "boobs",
-    "blowjob",
-    "hentai",
-    "tits",
-    "erok",
-    "feetg",
-    "bj",
-    "erokemo",
-    "kuni",
-    "les",
-    "trap",
-    "hololewd",
-    "lewdk",
-    "solog",
-    "pussy",
-    "yuri",
-    "lewdkemo",
-    "lewd",
-    "pwankg",
-    "eron",
-    "keta",
-    "eroyuri",
-    "holoero",
-    "classic",
-    "feet",
-    "gasm",
-    "spank",
-    "erofeet",
-    "ero",
-    "solo",
-    "cum",
-]
-
-sfw_tags_literal = Literal[
-    "tickle",
-    "ngif",
-    "meow",
-    "poke",
-    "kiss",
-    "8ball",
-    "lizard",
-    "slap",
-    "cuddle",
-    "goose",
-    "avatar",
-    "fox_girl",
-    "hug",
-    "gecg",
-    "pat",
-    "smug",
-    "kemonomimi",
-    "holo",
-    "wallpaper",
-    "woof",
-    "baka",
-    "feed",
-    "neko",
-    "waifu",
-]
-
-sfw_tags = get_args(sfw_tags_literal)
-nsfw_tags = get_args(nsfw_tags_literal)
-
-BASE_URL = "https://nekos.life/api/"
-VERSION = "v2"
-
+BASE_URL = "https://neko-love.xyz/api/"
+VERSION = "v1"
 URL = BASE_URL + VERSION
+
+sfw_tags = [
+    "neko",
+    "kitsune",
+    "hug",
+    "pat",
+    "waifu",
+    "cry",
+    "kiss",
+    "slap",
+    "smug",
+    "punch",
+]
+
+nsfw_tags = ["nekolewd"]
+
 
 main_embed = Embed(colour=0xC44BAB)
 main_embed.set_footer(
-    text="With Nekos.life",
-    icon_url="https://avatars.githubusercontent.com/u/34457007?s=200&v=4",
+    text="With Neko-love",
+    icon_url="https://neko-love.xyz/assets/icon-min.png",
 )
 
 anekos = RegisterCommand(name="네코", description="귀여운 네코미미를 보여줍니다. 태그를 사용해서 검색도 가능합니다.")
@@ -106,7 +47,7 @@ async def neko_help(request: HiyobotRequest):
 @anekos.sub_command(name="랜덤", description="랜덤으로 가져옵니다.")
 async def neko(request: HiyobotRequest):
     tag = choice(sfw_tags if not await is_nsfw(request) else nsfw_tags)
-    img_url = (await request.app.ctx.request.get(URL + f"/img/{tag}"))["url"]
+    img_url = (await request.app.ctx.request.get(URL + f"/{tag}"))["url"]
     return await request.ctx.response.send(embed=main_embed.set_image(url=img_url))
 
 
@@ -132,5 +73,5 @@ async def neko_search(request: HiyobotRequest, tag: str):
             return await request.ctx.response.send(
                 "해당 태그는 성인 태그인 것 같습니다. 연령 제한이 설정된 채널에서 사용해주세요."
             )
-    img_url = (await request.app.ctx.request.get(URL + f"/img/{tag}"))["url"]
+    img_url = (await request.app.ctx.request.get(URL + f"/{tag}"))["url"]
     return await request.ctx.response.send(embed=main_embed.set_image(url=img_url))
